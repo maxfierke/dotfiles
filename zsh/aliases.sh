05cf204bd87763cfad8fade042747d992acd6d8f
Max Fierke <max@maxfierke.com>
2017-06-15 22:15:03 -0500

    better zsh stuff

---
+++ zsh/aliases.sh
@@ -0,0 +1,7 @@
+alias be="bundle exec"
+alias bake="bundle exec bake"
+alias respek="bundle exec rspec"
+alias ect="ember test -s --launch Chrome"
+alias got=git
+alias fix_enter="stty sane"
+alias fix_webcam="sudo killall VDCAssistant"
