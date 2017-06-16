c0402532b6d07ff1680bf9346d2258eb353b23f1
Max Fierke <max@maxfierke.com>
2017-06-15 23:08:31 -0500

    Add iterm2 dynamic profiles

---
+++ iterm2/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
+cp $DOTFILES_ROOT/iterm2/profiles.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles
