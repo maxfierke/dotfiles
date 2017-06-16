7ae7211bf9db02e141681be3ab1ead7668cc9673
Max Fierke <max@maxfierke.com>
2017-06-15 23:05:27 -0500

    Fix some install problems

--- powerline-fonts/install.sh
+++ powerline-fonts/install.sh
@@ -4,10 +4,13 @@ set -e
 
 PL_FONTS_DIR=$DOTFILES_ROOT/vendor/powerline-fonts
 
-git clone https://github.com/powerline/fonts.git $PL_FONTS_DIR
+if ! [ -d $PL_FONTS_DIR ]; then
+    git clone https://github.com/powerline/fonts.git $PL_FONTS_DIR
+else
+    cd $PL_FONTS_DIR
+    git pull
+fi
 
 cd $PL_FONTS_DIR
 ./install.sh
 
-cd ../$PL_FONTS_DIR
-rm -rf $PL_FONTS_DIR
