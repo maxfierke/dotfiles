3884a8cfc9d7faa25cede47849977d8a2bb14663
Max Fierke <max@maxfierke.com>
2017-06-17 14:25:52 -0500

    Add standard step messaging

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+source $DOTFILES_ROOT/util/common.sh
 
-mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
-cp $DOTFILES_ROOT/iterm2/profiles.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles
+if [ "$(uname -s)" == "Darwin" ]; then
+    step 'Copying iTerm2 Profiles'
+    mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
+    cp $DOTFILES_ROOT/iterm2/profiles.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles
+fi
--- powerline-fonts/install.sh
+++ powerline-fonts/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -e
+source $DOTFILES_ROOT/util/common.sh
 
 PL_FONTS_DIR=$DOTFILES_ROOT/vendor/powerline-fonts
 
+
+step "Updating Powerline fonts"
 if ! [ -d $PL_FONTS_DIR ]; then
     git clone https://github.com/powerline/fonts.git $PL_FONTS_DIR
 else
@@ -11,6 +14,7 @@ else
     git pull
 fi
 
+step "Installing Powerline fonts"
 cd $PL_FONTS_DIR
 ./install.sh
 
