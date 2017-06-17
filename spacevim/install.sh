4a9f3e287389c4e39366ab3f307cf011e0fc7a46
Max Fierke <max@maxfierke.com>
2017-06-17 17:23:23 -0500

    Add build step message for spacevim

--- spacevim/install.sh
+++ spacevim/install.sh
@@ -4,5 +4,6 @@ set -e
 source $DOTFILES_ROOT/util/common.sh
 
 if [ ! -e "$HOME/.SpaceVim.d" ]; then
+    step 'Installing spacevim for neovim'
     curl -sLf https://spacevim.org/install.sh | bash -s -- install neovim
 fi
