8782ea84f89741ac7dad9e563c9ee47234637f05
Max Fierke <max@maxfierke.com>
2017-06-17 17:22:59 -0500

    Install bundler for all ruby versions

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,10 +3,16 @@
 set -e
 source $DOTFILES_ROOT/util/common.sh
 
+eval "$(rbenv init -)"
+
 install_ruby_versions() {
     for version in $(find $1 -name .ruby-version -maxdepth 2 | xargs cat | sort | uniq); do
         step "Installing ruby version $version"
         rbenv install --skip-existing $version
+        rbenv shell $version
+        rbenv rehash
+        gem install bundler
+        rbenv shell -
     done
 }
 
