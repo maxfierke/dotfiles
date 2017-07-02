ea07af5817beed494093519cba64e37f544a89fb
Max Fierke <max@maxfierke.com>
2017-07-02 13:52:38 -0500

    Ensure user gets a postgres db for their account

---
+++ postgresql/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Ensure createdb exists, or exit otherwise
+command -v createdb >/dev/null 2>&1 || exit
+
+# Create. That. DB!
+if [ "$(psql -tAc "SELECT 1 FROM pg_database WHERE datname='$USER'")" != '1' ]; then
+    createdb -e
+fi
+
