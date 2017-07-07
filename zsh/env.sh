11e817061171080f6e3d86c660ea6d2fc8510544
Max Fierke <max@maxfierke.com>
2017-07-07 10:16:51 -0500

    Fix nodenv and pyenv inconsistency with rbenv

---
+++ zsh/env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Ensure Homebrew rbenv/nodeenv/pyenv are setup correctly
+export RBENV_ROOT=/usr/local/opt/rbenv
+export NODENV_ROOT=/usr/local/opt/nodenv
+export PYENV_ROOT=/usr/local/opt/pyenv
+export PATH="$RBENV_ROOT/bin:$NODENV_ROOT/bin:$PYENV_ROOT/bin:$PATH"
+
+if command -v rbenv >/dev/null 2>&1; then eval "$(rbenv init -)"; fi
+if command -v nodenv >/dev/null 2>&1; then eval "$(nodenv init -)"; fi
+if command -v pyenv >/dev/null 2>&1; then eval "$(pyenv init -)"; fi
+
