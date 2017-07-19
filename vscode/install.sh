ef22dcea987c02084c8c8454f111f61616ebdbed
Max Fierke <max@maxfierke.com>
2017-07-18 22:40:16 -0500

    Install some more vscode packages

--- vscode/install.sh
+++ vscode/install.sh
@@ -10,20 +10,27 @@ command -v code >/dev/null 2>&1 || exit
 ln -sfv "$DOTFILES_ROOT/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
 VSCODE_PACKAGES=(
+  ms-vscode.cpptools
   g3ortega.crystal
   kofno.crystal-ide
   stevejpurves.cucumber
   msjsdiag.debugger-for-chrome
+  EditorConfig.editorconfig
+  sbrink.elm
   codezombiech.gitignore
   tberman.json-schema-validator
   PKief.material-icon-theme
+  felixfbecker.php-pack
   ricard.postcss
   mohsen1.prettify-json
   rebornix.ruby
   shardulm94.trailing-spaces
+  whatwedo.twig
   mjmcloug.vscode-elixir
   dbaeumer.vscode-eslint
+  felixrieseberg.vsc-ember-cli
   Equinusocio.vsc-material-theme
+  emberjs.vscode-ember
 )
 
 for package in "${VSCODE_PACKAGES[@]}"; do
