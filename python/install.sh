3a1603e30cd76197d892673a983087eb291c48e8
Max Fierke <max@maxfierke.com>
2017-10-12 22:29:37 -0500

    Guess 10.13 needs CFLAGS and LDFLAGS for openssl when pyenv installing now :/

--- python/install.sh
+++ python/install.sh
@@ -8,6 +8,11 @@ source $DOTFILES_ROOT/util/common.sh
 
 eval "$(pyenv init -)"
 
+if [ -x "$(command -v brew)" ]; then
+    export CFLAGS="-I$(brew --prefix openssl)/include"
+    export LDFLAGS="-L$(brew --prefix openssl)/lib"
+fi
+
 install_python_versions() {
     for version in $(find $1 -name .python-version -maxdepth 2 | xargs cat | sort | uniq); do
         step "Installing python version $version"
