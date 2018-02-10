8dbdf841851be8594d62108d2a644d5936392d30
Max Fierke <max@maxfierke.com>
2018-02-09 19:49:08 -0600

    Add cargo to env

--- zsh/env.sh
+++ zsh/env.sh
@@ -11,4 +11,4 @@ export PATH="$RBENV_ROOT/bin:$NODENV_ROOT/bin:$PYENV_ROOT/bin:/usr/local/sbin:$P
 if command -v rbenv >/dev/null 2>&1; then eval "$(rbenv init -)"; fi
 if command -v nodenv >/dev/null 2>&1; then eval "$(nodenv init -)"; fi
 if command -v pyenv >/dev/null 2>&1; then eval "$(pyenv init -)"; fi
-
+if [ -d "$HOME/.cargo/env" ]; then source "$HOME/.cargo/env"; fi
