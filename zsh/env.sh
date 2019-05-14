75f0f4684b1dc59c60e68dbe126f49018b737e44
Max Fierke <max.fierke@iorahealth.com>
2019-05-14 17:01:59 -0500

    Avoid the _ROOT params, beloved

--- zsh/env.sh
+++ zsh/env.sh
@@ -2,11 +2,14 @@
 
 export DOTFILES_ROOT="$HOME/.dotfiles"
 
-# Ensure Homebrew rbenv/nodeenv/pyenv are setup correctly
-export RBENV_ROOT=/usr/local/opt/rbenv
-export NODENV_ROOT=/usr/local/opt/nodenv
-export PYENV_ROOT=/usr/local/opt/pyenv
-export PATH="$RBENV_ROOT/bin:$NODENV_ROOT/bin:$PYENV_ROOT/bin:/usr/local/sbin:$PATH"
+if [ -z "$CHIRPSTRAPPED" ]; then
+  # Ensure Homebrew rbenv/nodeenv/pyenv are setup correctly
+  # DEPRECATED: probably going to get rid of this
+  export RBENV_ROOT=/usr/local/opt/rbenv
+  export NODENV_ROOT=/usr/local/opt/nodenv
+  export PYENV_ROOT=/usr/local/opt/pyenv
+  export PATH="$RBENV_ROOT/bin:$NODENV_ROOT/bin:$PYENV_ROOT/bin:/usr/local/sbin:$PATH"
+fi
 
 if command -v rbenv >/dev/null 2>&1; then eval "$(rbenv init -)"; fi
 if command -v nodenv >/dev/null 2>&1; then eval "$(nodenv init -)"; fi
