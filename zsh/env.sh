bf9003e65bccdef542ed21eaf8aa7f42c189fd9f
Max Fierke <max.fierke@iorahealth.com>
2019-05-14 17:04:39 -0500

    Just get rid of the _ROOTs

--- zsh/env.sh
+++ zsh/env.sh
@@ -2,15 +2,6 @@
 
 export DOTFILES_ROOT="$HOME/.dotfiles"
 
-if [ -z "$CHIRPSTRAPPED" ]; then
-  # Ensure Homebrew rbenv/nodeenv/pyenv are setup correctly
-  # DEPRECATED: probably going to get rid of this
-  export RBENV_ROOT=/usr/local/opt/rbenv
-  export NODENV_ROOT=/usr/local/opt/nodenv
-  export PYENV_ROOT=/usr/local/opt/pyenv
-  export PATH="$RBENV_ROOT/bin:$NODENV_ROOT/bin:$PYENV_ROOT/bin:/usr/local/sbin:$PATH"
-fi
-
 if command -v rbenv >/dev/null 2>&1; then eval "$(rbenv init -)"; fi
 if command -v nodenv >/dev/null 2>&1; then eval "$(nodenv init -)"; fi
 if command -v pyenv >/dev/null 2>&1; then eval "$(pyenv init -)"; fi
