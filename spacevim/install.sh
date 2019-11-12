6f2a3366e8286f73ca4b3f84a91bf1214ffd9115
Max Fierke <max@maxfierke.com>
2019-11-11 22:57:09 -0600

    Make neovim & spacevim install work with asdf

--- spacevim/install.sh
+++ spacevim/install.sh
@@ -8,8 +8,6 @@ if [ ! -d "$HOME/.SpaceVim.d" ]; then
     curl -sLf https://spacevim.org/install.sh | bash -s -- install neovim
 fi
 
-eval "$(pyenv init -)"
-
 if [ -x "/usr/local/bin/pip2" ]; then
     /usr/local/bin/pip2 install -U neovim
 fi
@@ -18,10 +16,32 @@ if [ -x "/usr/local/bin/pip3" ]; then
     /usr/local/bin/pip3 install -U neovim
 fi
 
-pythons=$(pyenv versions --bare)
+
+if command -v pyenv 2>&1 > /dev/null; then
+    PY_VERSION_MANAGER="pyenv"
+elif command -v asdf 2>&1 > /dev/null; then
+    PY_VERSION_MANAGER="asdf"
+else
+    exit
+fi
+
+if [ "$PY_VERSION_MANAGER" == "pyenv" ]; then
+    eval "$(pyenv init -)"
+    pythons=$(pyenv versions --bare)
+elif [ "$PY_VERSION_MANAGER" == "asdf" ]; then
+    source $(brew --prefix asdf)/asdf.sh
+    pythons=$(asdf list python | grep -v 'No versions installed')
+else
+    exit
+fi
 
 for python in $pythons; do
-    pyenv shell $python
-    pyenv rehash
-    pyenv exec pip install -U neovim
+    if [ "$PY_VERSION_MANAGER" == "pyenv" ]; then
+        pyenv shell $python
+        pyenv rehash
+        pyenv exec pip install -U neovim
+    else
+        ASDF_PYTHON_VERSION=$python asdf reshim pip $python
+        ASDF_PYTHON_VERSION=$python asdf exec pip install -U neovim
+    fi
 done
