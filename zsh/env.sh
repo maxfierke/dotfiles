74712b0a240fefd7fd11ce9179d353cb2f79c3a2
Max Fierke <max@maxfierke.com>
2020-05-02 13:55:55 -0500

    Ensure snap path is added on Wayland

--- zsh/env.sh
+++ zsh/env.sh
@@ -2,6 +2,14 @@
 
 export DOTFILES_ROOT="$HOME/.dotfiles"
 
+if command -v snap >/dev/null 2>&1; then
+    if echo $PATH | grep -q ':/snap/bin'; then
+    else
+        # Add snap bin to PATH if it wasn't set correctly by OS
+        export PATH="$PATH:/snap/bin"
+    fi
+fi
+
 if command -v rbenv >/dev/null 2>&1; then eval "$(rbenv init -)"; fi
 if command -v nodenv >/dev/null 2>&1; then eval "$(nodenv init -)"; fi
 if command -v pyenv >/dev/null 2>&1; then eval "$(pyenv init -)"; fi
