7a14f3bdb2903991d479b101ce0d4df8bffe2b38
Max Fierke <max@maxfierke.com>
2023-10-13 16:06:44 -0500

    Add homebrew-loading hook if I'm sadly without my beloved bootstrapping tools

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if ! command -v brew >/dev/null 2>&1; then
+    HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-}
+    if [ -d "/opt/homebrew" ]; then
+        HOMEBREW_PREFIX=/opt/homebrew
+    elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+        HOMEBREW_PREFIX=/home/linuxbrew/.linuxbrew
+    fi
+
+    if [ ! -z "$HOMEBREW_PREFIX" ]; then
+        eval $($HOMEBREW_PREFIX/bin/brew shellenv)
+    fi
+fi
+
 if command -v snap >/dev/null 2>&1; then
     if ! echo "$PATH" | grep -q ':/snap/bin'; then
         # Add snap bin to PATH if it wasn't set correctly by OS
